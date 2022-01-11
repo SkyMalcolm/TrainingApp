0364ccbd9ef142327c24bae26695583702eaccb8
SkyMalcolm <sky.lennartsson@iths.se>
2022-01-11 13:04:15 +0100

    Initial Commit

---
+++ TrainingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrainingApp
+//
+//  Created by Malcolm Lennartsson on 2022-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrainingApp/TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppApp.swift
+//  TrainingApp
+//
+//  Created by Malcolm Lennartsson on 2022-01-11.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
